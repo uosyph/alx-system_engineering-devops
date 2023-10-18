cbd09ee364ecd187cf19c0e83a9a036236183bf8
Yousef Saeed <yousafesaeed@gmail.com>
2023-10-18 19:01:28 +0300

    0x1B - 0,1

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Fixes the number of failed requests to 0
+
+service { 'nginx stop':
+  ensure => stopped;
+}
+
+exec { 'fix--for-nginx':
+  command => "sed -i 's/15/2000/g' /etc/default/nginx",
+  path    => ['/bin'],
+}
+
+service { 'nginx':
+  ensure => running,
+}
